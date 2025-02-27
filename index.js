2dbc00f73a1eae7ac13be4a87fb8eead802c735e
alcmarfil <acmarfil@up.edu.ph>
2025-02-27 14:40:09 +0800

    docs: added comments to index.js

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
-const { v4: uuidv4 } = require('uuid');
-var validator = require('validator');
-const { appendFileSync } = require('node:fs');
+// const { v4: uuidv4 } = require('uuid');
+// var validator = require('validator');
+// const { appendFileSync } = require('node:fs');
+import { v4 as uuidv4 } from 'uuid';
+import validator from 'validator';
+import { appendFileSync } from 'node:fs';
 
 function generateUniqueID(firstname, lastname){
-  if (!firstname || !lastname){
+  if (!firstname || !lastname){ //check first if null 
     return null;
   }
-  let lowercaseFirstname = firstname.toLowerCase();
+
+  //make it lowercase 
+  let lowercaseFirstname = firstname.toLowerCase(); 
   let lowercaseLastname = lastname.toLowerCase();
-  let firstletter = lowercaseFirstname.charAt(0);
+  let firstletter = lowercaseFirstname.charAt(0); //get the first letter 
 
-  const uid = uuidv4().substring(0,8);
-  console.log (uid);
+  const uid = uuidv4().substring(0,8); //get only 8 characters of the string 
+  //console.log (uid);
 
-  return firstletter+lowercaseLastname+uid
+  return firstletter+lowercaseLastname+uid //return the uid 
 
 }
 
-console.log(generateUniqueID("Athalia", "Marfil"))
 
 function addAccount(userinfo){
-  if (userinfo.length != 4){
-    console.log(1)
+  if (userinfo.length !== 4){ //if something is missing return null 
+   // console.log(1)
     return false;
   }
 
-  console.log(userinfo)
+  //console.log(userinfo)
   let [firstname, lastname, email, age] = userinfo
   
 
-  if (!firstname || !lastname || !email){
-    console.log(2)
+  if (!firstname || !lastname || !email){ //check if it is null
+   // console.log(2)
     return false;
   }
     
     
-  if (!validator.isEmail(email) || age < 18){
-    console.log(3)
+  if (!validator.isEmail(email) || age < 18){ //check if email is valid and age is above 18
+   // console.log(3)
     return false;
   }
 
-  const uniqueID = generateUniqueID(firstname, lastname);
-  if (!uniqueID){
-    console.log(4)
+  const uniqueID = generateUniqueID(firstname, lastname); // generate unique id 
+
+  if (!uniqueID){ //if uid is null return false
+   // console.log(4)
     return false; 
   }
 
-  const userData = firstname+','+lastname+','+email+','+ age +',' + uniqueID+"\n";
+  const userData = firstname+','+lastname+','+email+','+ age +',' + uniqueID+"\n"; 
 
+  // save to file 
   try {
-    appendFileSync('users.txt', userData);
-    console.log('The "data to append" was appended to file!');
+    appendFileSync('users.txt', userData); 
+  //  console.log('The "data to append" was appended to file!');
     return true;
   } catch (err) {
     return false;
@@ -58,14 +64,5 @@ function addAccount(userinfo){
 
 }
 
+export {addAccount, generateUniqueID}
 
-//VALID
-console.log(addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));
-addAccount(["Athalia", "Marfil", "acmarfil@up.edu.ph", 20]);
-
-//INVALID
-console.log(addAccount(["", "Turing", "aturing@w3c.com", 58]));
-console.log(addAccount(["Athalia", "Lexine", "aturing@w3c.com"]));
-console.log(addAccount(["Athalia", "Marfil", "HELLO", 20]));
-console.log(addAccount(["Athalia", "Marfil", "HELLO", 10]));
-console.log(addAccount(["Athalia", "", "HELLO", 10]));
